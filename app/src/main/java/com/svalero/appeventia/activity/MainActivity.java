16ccecfb68447492643735e68b559651cd4e8627
LF27819 <luciafeliperuiz@gmail.com>
2026-05-19 09:40:19 +0200

    Añadir navegación a listado de eventos

--- app/src/main/java/com/svalero/appeventia/activity/MainActivity.java
+++ app/src/main/java/com/svalero/appeventia/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.svalero.appeventia.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button eventsButton = findViewById(R.id.eventsButton);
+
+        eventsButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, EventoListActivity.class);
+            startActivity(intent);
+        });
     }
 }
