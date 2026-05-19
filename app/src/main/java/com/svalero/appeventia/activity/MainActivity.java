c7181ab57380a6ce6065af19c6dec3a02a97d4af
LF27819 <luciafeliperuiz@gmail.com>
2026-05-19 09:10:36 +0200

    Crear estructura inicial Android

--- app/src/main/java/com/svalero/appeventia/activity/MainActivity.java
+++ app/src/main/java/com/svalero/appeventia/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.svalero.appeventia;
+package com.svalero.appeventia.activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.svalero.appeventia.R;
 
 public class MainActivity extends AppCompatActivity {
 
