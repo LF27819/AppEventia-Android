56de054d4a417faa951ed5170d0dcf7276d2365d
LF27819 <luciafeliperuiz@gmail.com>
2026-05-20 19:54:01 +0200

    Menu de opciones

--- app/src/main/java/com/svalero/appeventia/view/MainActivity.java
+++ app/src/main/java/com/svalero/appeventia/view/MainActivity.java
@@ -8,6 +8,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.svalero.appeventia.R;
 
+import android.view.Menu;
+import android.view.MenuItem;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -46,5 +49,42 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(MainActivity.this, ReservaListActivity.class);
             startActivity(intent);
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_events) {
+            startActivity(new Intent(this, EventoListActivity.class));
+            return true;
+        }
+
+        if (id == R.id.menu_artists) {
+            startActivity(new Intent(this, ArtistaListActivity.class));
+            return true;
+        }
+
+        if (id == R.id.menu_reservations) {
+            startActivity(new Intent(this, ReservaListActivity.class));
+            return true;
+        }
+
+        if (id == R.id.menu_favorites) {
+            startActivity(new Intent(this, FavoritesActivity.class));
+            return true;
+        }
+
+        if (id == R.id.menu_profile) {
+            startActivity(new Intent(this, ProfileActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
